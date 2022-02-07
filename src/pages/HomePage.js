ef04c555b7492c2b1e90814b769766670c298903
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-07 10:27:04 -0800

    initial commit and HomePage.js added

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const HomePage = () => {
+	return (
+		<>
+			<h1>Hello, welcome to my blog!</h1>
+			<p>
+				Pokem ipsum dolor sit amet Misty to catch them is my real test et dolore
+				magna aliqua Axew Absol Hydro Pump. Leech Life Galvantula Chingling
+				Mienfoo Houndour Grimer Cottonee. Ut aliquip ex ea commodo consequat
+				Kricketune Virizion Luxray Hitmonchan Pokedex Gligar. Rising Badge
+				Croconaw Venomoth Elgyem Dragonite Zigzagoon Conkeldurr. Dig blast off
+				at the speed of light Plusle Team Rocket Kyogre Buneary Geodude.
+			</p>
+			<p>
+				Dragon Rage Dunsparce Dig Galvantula Volcarona Aerodactyl Lugia. Pallet
+				Town Simisage Nurse Joy Klink Cradily Quagsire Seedot. Scratch Monferno
+				Seadra Roselia Roggenrola Shellder Durant.{' '}
+			</p>
+		</>
+	)
+}
+
+export default HomePage
