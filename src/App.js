5aadb0a32caf44c30d452b5b7cee838b23b65fc0
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-07 12:06:11 -0800

    added more pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import HomePage from './pages/HomePage'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 	return (
-		<div className='container p-2'>
-			<HomePage />
-		</div>
+		<Router>
+			<div className='container p-2'>
+				<Routes>
+					<Route path='/' element={<HomePage />} exact />
+				</Routes>
+			</div>
+		</Router>
 	)
 }
 
