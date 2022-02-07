249e695071d904f595b7f73abe95e5333700a141
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-07 11:16:42 -0800

    installed tailwind

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HomePage = () => {
 	return (
-		<>
-			<h1>Hello, welcome to my blog!</h1>
-			<p>
+		<div>
+			<h1 className='text-3xl mb-3'>Hello, welcome to my blog!</h1>
+			<p className='mb-3'>
 				Pokem ipsum dolor sit amet Misty to catch them is my real test et dolore
 				magna aliqua Axew Absol Hydro Pump. Leech Life Galvantula Chingling
 				Mienfoo Houndour Grimer Cottonee. Ut aliquip ex ea commodo consequat
@@ -17,7 +17,7 @@ const HomePage = () => {
 				Town Simisage Nurse Joy Klink Cradily Quagsire Seedot. Scratch Monferno
 				Seadra Roselia Roggenrola Shellder Durant.{' '}
 			</p>
-		</>
+		</div>
 	)
 }
 
