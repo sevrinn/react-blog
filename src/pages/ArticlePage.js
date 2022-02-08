5e81a65a0f1171d91abd6452697b7625c714c7aa
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-07 17:56:36 -0800

    added article-content and mapped paragraphs

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import articleContent from './article-content'
 const ArticlePage = () => {
 	const { name } = useParams()
+	const article = articleContent.find((article) => article.name === name)
+
+	if (!article) return <h1 className='text-3xl mb-3'>Article does not exist</h1>
 	return (
 		<>
-			<h1 className='text-3xl mb-3'>This is the {name} article</h1>
+			<h1 className='text-3xl mb-3'>{article.title}</h1>
+			{article.content.map((paragraph, key) => (
+				<p key={key} className='mb-5'>
+					{paragraph}
+				</p>
+			))}
 		</>
 	)
 }
