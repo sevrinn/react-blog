2d6fb046ca3acdac08a1e891924b8212b7031042
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-08 06:37:09 -0800

    lists other articles at bottom of each ArticlePage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import articleContent from './article-content'
-const ArticlePage = () => {
-	const { name } = useParams()
-	const article = articleContent.find((article) => article.name === name)
+import ArticlesList from '../components/ArticlesList'
 
-	if (!article) return <h1 className='text-3xl mb-3'>Article does not exist</h1>
-	return (
-		<>
-			<h1 className='text-3xl mb-3'>{article.title}</h1>
-			{article.content.map((paragraph, key) => (
-				<p key={key} className='mb-5'>
-					{paragraph}
-				</p>
-			))}
-		</>
-	)
+const ArticlePage = () => {
+  const { name } = useParams()
+  const article = articleContent.find((article) => article.name === name)
+
+  if (!article) return <h1 className='text-3xl mb-3'>Article does not exist</h1>
+
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  )
+  return (
+    <>
+      <h1 className='text-3xl mb-3'>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key} className='mb-5'>
+          {paragraph}
+        </p>
+      ))}
+      <h3>Other Articles</h3>
+      <ArticlesList articles={otherArticles} />
+    </>
+  )
 }
 
 export default ArticlePage
