0c5d30b93fed6473c78f85549fc20a8a8dcb1bf3
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-09 11:47:22 -0800

    refactored article and upvote endpoints

--- server/src/server.js
+++ server/src/server.js
@@ -6,35 +6,37 @@ const app = express()
 
 app.use(bodyParser.json())
 
-//ARTICLE
-app.get('/api/articles/:name', async (req, res) => {
+const withDB = async (operations, res) => {
 	try {
-		const articleName = req.params.name
 		const client = await MongoClient.connect('mongodb://localhost:27017', {
 			useNewUrlParser: true,
 		})
 		const db = client.db('my-blog')
 
-		const articleInfo = await db
-			.collection('articles')
-			.findOne({ name: articleName })
-		res.status(200).json(articleInfo)
+		await operations(db)
 
 		client.close()
 	} catch (error) {
 		res.status(500).json({ message: 'Error connecting to db', error })
 	}
+}
+
+//ARTICLE
+app.get('/api/articles/:name', async (req, res) => {
+	withDB(async (db) => {
+		const articleName = req.params.name
+
+		const articleInfo = await db
+			.collection('articles')
+			.findOne({ name: articleName })
+		res.status(200).json(articleInfo)
+	}, res)
 })
 
 //UPVOTES
 app.post('/api/articles/:name/upvote', async (req, res) => {
-	try {
-		const articleName = req.params.name
-		const client = await MongoClient.connect('mongodb://localhost:27017', {
-			useNewUrlParser: true,
-		})
-		const db = client.db('my-blog')
-
+	const articleName = req.params.name
+	withDB(async (db) => {
 		const articleInfo = await db
 			.collection('articles')
 			.findOne({ name: articleName })
@@ -51,11 +53,7 @@ app.post('/api/articles/:name/upvote', async (req, res) => {
 			.findOne({ name: articleName })
 
 		res.status(200).json(updatedArticleInfo)
-
-		client.close()
-	} catch (error) {
-		res.status(500).json({ message: 'Error connecting to db', error })
-	}
+	}, res)
 })
 
 //COMMENTS
