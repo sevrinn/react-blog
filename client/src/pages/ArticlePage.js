189563e5bb4e5961decac4393de56235b1704c86
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-09 17:40:01 -0800

    added comments list to bottom of article page

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import articleContent from './article-content'
 import NotFoundPage from './NotFoundPage'
 import ArticlesList from '../components/ArticlesList'
+import CommentsList from '../components/CommentsList'
 
 const ArticlePage = () => {
 	const { name } = useParams()
 	const article = articleContent.find((article) => article.name === name)
+
+	const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetch(`/api/articles/${name}`)
+			const body = await result.json()
+			setArticleInfo(body)
+		}
+		fetchData()
+	}, [name])
 
 	if (!article) return <NotFoundPage />
 
@@ -16,11 +28,13 @@ const ArticlePage = () => {
 	return (
 		<>
 			<h1 className='text-3xl mb-3'>{article.title}</h1>
+			<p>This post has been upvoted {articleInfo.upvotes} times</p>
 			{article.content.map((paragraph, key) => (
 				<p key={key} className='mb-5'>
 					{paragraph}
 				</p>
 			))}
+			<CommentsList comments={articleInfo.comments} />
 			<h3>Other Articles</h3>
 			<ArticlesList articles={otherArticles} />
 		</>
