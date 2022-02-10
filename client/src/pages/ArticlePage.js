841be551ab7e886c752c364474342de0ab478ab9
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-09 16:25:07 -0800

    installed packaged so internet explorer can handle fetch

--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -5,26 +5,26 @@ import NotFoundPage from './NotFoundPage'
 import ArticlesList from '../components/ArticlesList'
 
 const ArticlePage = () => {
-  const { name } = useParams()
-  const article = articleContent.find((article) => article.name === name)
+	const { name } = useParams()
+	const article = articleContent.find((article) => article.name === name)
 
-  if (!article) return <NotFoundPage />
+	if (!article) return <NotFoundPage />
 
-  const otherArticles = articleContent.filter(
-    (article) => article.name !== name
-  )
-  return (
-    <>
-      <h1 className='text-3xl mb-3'>{article.title}</h1>
-      {article.content.map((paragraph, key) => (
-        <p key={key} className='mb-5'>
-          {paragraph}
-        </p>
-      ))}
-      <h3>Other Articles</h3>
-      <ArticlesList articles={otherArticles} />
-    </>
-  )
+	const otherArticles = articleContent.filter(
+		(article) => article.name !== name
+	)
+	return (
+		<>
+			<h1 className='text-3xl mb-3'>{article.title}</h1>
+			{article.content.map((paragraph, key) => (
+				<p key={key} className='mb-5'>
+					{paragraph}
+				</p>
+			))}
+			<h3>Other Articles</h3>
+			<ArticlesList articles={otherArticles} />
+		</>
+	)
 }
 
 export default ArticlePage
