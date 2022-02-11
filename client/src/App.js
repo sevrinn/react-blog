ed865b96704ded8e8c115f397ff785ef1624b398
Sevrinn <sevrinnsevrinn@gmail.com>
2022-02-10 20:05:23 -0800

    added styles to comment form

--- client/src/App.js
+++ client/src/App.js
@@ -8,22 +8,22 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <div className='container p-20'>
-          <Routes>
-            <Route path='/' element={<HomePage />} exact />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/article-list' element={<ArticleListPage />} />
-            <Route path='/article/:name' element={<ArticlePage />} />
-            <Route path='*' element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  )
+	return (
+		<Router>
+			<div className='box-border'>
+				<Navbar />
+				<div className='container p-20'>
+					<Routes>
+						<Route path='/' element={<HomePage />} exact />
+						<Route path='/about' element={<AboutPage />} />
+						<Route path='/article-list' element={<ArticleListPage />} />
+						<Route path='/article/:name' element={<ArticlePage />} />
+						<Route path='*' element={<NotFoundPage />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	)
 }
 
 export default App
